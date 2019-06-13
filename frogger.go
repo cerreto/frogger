1d684a74749ff620d0a261e97fc25333cf5624ff
Alessandro Bedogni <allebedo01@gmail.com>
2019-06-13 17:57:06 +0200

    added logs

--- frogger.go
+++ frogger.go
@@ -5,13 +5,14 @@ import "strconv"
 var bestSc = 0
 var Sc = 0
 var h,w=512,448
-var screen=Size{h,w}
+var screen=Size{w,h}
 var x,y=15,15
 var vx,vy=5,5
 var frog=LoadImage("./assets/frog.png")
 var car1=LoadImage("./assets/truck-right.png")
 var car2=LoadImage("./assets/car2-left.png")
 var car3=LoadImage("./assets/car3-left.png")
+var log=LoadImage("./assets/log.png")
 
 //entity class
 type ent struct {
@@ -19,43 +20,61 @@ type ent struct {
     h, w    int
     ix,iy 	int
     dx, dy  int
+    log 	bool
 }
 
+
 //creates entities
-func newent(pos Point, h,w int, ix,iy int, dx,dy int) *ent {
-    b := &ent{pos.X, pos.Y, h, w, ix,iy, dx, dy}
+func newent(pos Point, h,w int, ix,iy int, dx,dy int, log bool) *ent {
+    b := &ent{pos.X, pos.Y, h, w, ix,iy, dx, dy,log}
     return b
 }
 
 //moves entities
 func (b *ent) Move() {
     if b.x>(w+200) {
-        b.x=-50
+        b.x=-100
     }
     if b.x<(-200) {
-        b.x=w+50
+        b.x=w+100
     }
     b.x += b.dx
-    if (b.x <= fr.x && fr.x<=b.x+b.w) && b.y==fr.y {
-    	fr.x=fr.ix
-    	fr.y=fr.iy
-    	Sc = 0
-    }
-}
+	if (fr.x>b.x && fr.x<b.x+b.w) && fr.y==b.y {
+	   if b.log==false {
+	   	fr.x=fr.ix
+	   	fr.y=fr.iy
+	    Sc = 0
+	   } else {
+	   	fr.x+=b.dx
+	   	if fr.x<0 || fr.x+fr.w>w {
+	   		fr.x=fr.ix
+	   		fr.y=fr.iy
+	    	Sc = 0
+	   	}
+	   }
+	   }
+	}
 
 func environment() {
 	b1.Move()
 	b2.Move()
 	b3.Move()
+	l1.Move()
+	l2.Move()
 	DrawImage(car1,Point{b1.x,b1.y})
 	DrawImage(car2,Point{b2.x,b2.y})
 	DrawImage(car3,Point{b3.x,b3.y})
+	DrawImage(log,Point{l1.x,l1.y})
+	DrawImage(log,Point{l2.x,l2.y})
 }
 
-var b1=newent(Point{70,h*10/32},64,32,70,320,10,0)
-var b2=newent(Point{50,h*12/32},32,32,50,480,-10,0)
-var b3=newent(Point{40,h*14/32},32,32,40,544,-5,0)
-var fr=newent(Point{(w+32)/2,h*26/32},32,32,(w+32)/2,h*26/32,0,0)
+var b1=newent(Point{70,h*10/32},32,64,70,h*10/32,10,0,false)
+var b2=newent(Point{50,h*12/32},32,32,50,h*12/32,-10,0,false)
+var b3=newent(Point{40,h*14/32},32,32,40,h*14/32,-5,0,false)
+var fr=newent(Point{(w+32)/2,h*26/32},32,32,(w+32)/2,h*26/32,0,0,false)
+var l1=newent(Point{100,h*6/32},32,96,100,h*6/32,-3,0,true)
+var l2=newent(Point{200,h*4/32},32,96,200,h*4/32,3,0,true)
+
 var jump=32
 func tic() {
 	ClearCanvas()
@@ -64,13 +83,13 @@ func tic() {
 		 fr.y-=jump
 	}
 	if KeyPressed("s") || KeyPressed("ArrowDown") {
-		if (fr.y+fr.h+jump <= h-fr.h-jump) {fr.y+=jump}
+		if (fr.y+fr.h+jump<=h) {fr.y+=jump}
 	}
 	if KeyPressed("a") || KeyPressed("ArrowLeft") {
-		if (0<fr.x -(fr.w)/2&& fr.x<w-fr.w-fr.w) {fr.x-=jump}
+		if (0<=fr.x-jump) {fr.x-=jump}
 	}
 	if KeyPressed("d") || KeyPressed("ArrowRight") {
-		if (0<fr.x && fr.x<w-jump-fr.w) {fr.x+=jump}
+		if (fr.x+fr.w+jump<=w) {fr.x+=jump}
 	}
 	if fr.y+fr.h<w*15/100 {
 		Sc ++
@@ -81,8 +100,8 @@ func tic() {
 		}
 	}
 	DrawImage(frog,Point{fr.x,fr.y})
-	DrawText("Best Score:"+strconv.Itoa(bestSc), Point{w*86/100, h*77/100}, w*4/100)
-	DrawText("Score:" + strconv.Itoa(Sc), Point{w*86/100, h*80/100}, w*4/100)
+	DrawText("Best Score:"+strconv.Itoa(bestSc), Point{w*70/100, h*85/100}, w*4/100)
+	DrawText("Score:" + strconv.Itoa(Sc), Point{w*70/100, h*90/100}, w*4/100)
 	DrawLine(Point{0, w*86/100}, Point{h, w*86/100})
 
 	for i := 0; i < 32; i+=2 {
@@ -93,6 +112,7 @@ func tic() {
 
 func main() {
 	InitCanvas(screen)
+	Println(h,w)
 	SetFrameRate(60)
 	SetColor(Color{0,0,0})
 	MainLoop(tic)
